cf8475dddfe3624c54543f4e69e5a1a1481d7346
DarshanMS8197 <darshanms101@gmail.com>
2026-02-06 10:58:08 +0530

    meow moew

--- playwright.config.js
+++ playwright.config.js
@@ -52,7 +52,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+      //test
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
